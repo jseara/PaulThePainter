fde46998477784ab96965225354569eadf2f906f
Justin <jseara2011@gmail.com>
2018-01-22 15:08:26 -0500

    Moving To New Repository
    Because I can't write a decent .gitignore for the life of me

---
+++ Assets/Scripts/PlayerInteractions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour 
+{
+	// Use this for initialization
+	void onClick()
+	{
+		//if(Input.GetKeyDown(key: "G"))
+		//{
+			//Pick-up items
+
+			//Send out raycast, nearest item to the player is the one interacted with
+
+			//if(item == Key)
+				//pickup and add to inventory
+			//if(item == bucket)
+				//pickup and add to visible inventory
+			//if(item == well)
+				//remove all current coats of paint
+		//}
+		//if(Input.GetKeyDown(key: "E"))
+		//{
+			//Player applies coat of paint
+			//Display progress bar that updates
+			//Upon update completion, do the following
+			//Check if previous coat of paint was present
+				//If yes, apply mix between two colors
+				//If no, apply color
+		//}
+	}
+
+}
